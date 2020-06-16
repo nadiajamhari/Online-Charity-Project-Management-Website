84782de189a2beefabdabcbd58e624bca8b46c6b
nadiajamhari <nadiajamhari.15@gmal.com>
2020-06-16 14:16:30 +0800

    Update project.php
    change style css import

--- project.php
+++ project.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="layout.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="project.css">
+    <link href="project.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
